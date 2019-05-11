2ec5047ba627aecbb96ed1040c41837083ab1380
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-05-11 15:40:31 +0200

    AdminPanel component with test added

---
+++ src/components/AdminPanel.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AdminPanel extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Admin Panel
+            </div>
+        )
+    }
+}
+
+export default AdminPanel;
---
+++ src/components/AdminPanel.test.js
@@ -0,0 +1,20 @@
+import ReactDOM from "react-dom";
+import AdminPanel from "./AdminPanel";
+import React from 'react';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({adapter: new Adapter()});
+
+describe('AdminPanel test', () => {
+    it('AdminPanel renders with no problems ', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<AdminPanel/>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('Inventory rendered', () => {
+        const wrapper = shallow(<AdminPanel/>);
+        expect(wrapper.find('div').text()).toBe('Admin Panel');
+    })
+});

e5ce2d1d66bc4eff092cf4d29305e7279b876e39
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-05-11 17:29:20 +0200

    State introduced to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,49 @@ import React from 'react';
 
 class Header extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            bookstoreName: 'Magic books',
+            clicked: true,
+            textColor: 'white',
+            backgroundColor: 'black'
+        }
+    }
+
+    handleClicked = () => {
+
+        if (this.state.clicked) {
+            this.setState({
+                bookstoreName: 'Black books',
+                textColor: 'black',
+                backgroundColor: 'white'
+            })
+        } else {
+            this.setState({
+                bookstoreName: 'Magic books',
+                textColor: 'white',
+                backgroundColor: 'black'
+            })
+        }
+        this.setState({
+            clicked: !this.state.clicked
+        })
+    };
+
+
     render() {
+
+        let headerCss = {
+            color: this.state.textColor,
+            backgroundColor: this.state.backgroundColor
+        };
+
         return (
-            <div className='row header'>
-                <h1>
-                    Header
-                </h1>
+            <div className='row header' style={headerCss} onClick={this.handleClicked}>
+                <h1>{this.state.bookstoreName}</h1>
             </div>
-        )
+        );
     }
 }
 

0d7e7825601baa95d9c6710ea7cebb3fe40918c8
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-05-11 16:26:38 +0200

    Basic css added to index.css

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,13 @@ import AdminPanel from './AdminPanel';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
+import '../index.css';
 
 class App extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className='app container'>
                 <Header/>
                 <Order/>
                 <Inventory/>

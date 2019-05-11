369e4dd31f42f587f747a04bf7c48ecb1f5c60d7
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-05-11 15:04:31 +0200

    Enzyme library added

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,10 +1,18 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 import React from 'react';
+import {configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
 
+configure({adapter: new Adapter()});
 
-it('App renders with no problems ',()=>{
-    const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('App test', () => {
+    it('App renders with no problems ', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App/>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
 });
+
+

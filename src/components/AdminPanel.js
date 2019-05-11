7c3d65dc53338865e13746d8d3a777917033ba24
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-05-11 18:40:43 +0200

    Form added to AdminPanel

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -3,9 +3,33 @@ import React from 'react';
 class AdminPanel extends React.Component {
 
     render() {
+
         return (
             <div className='adminPanel col-md-4'>
-                Admin Panel
+                <form>
+                    <div className='form-group'>
+                        <input type='text' placeholder='Book name' id='book' name='name'
+                               className='form-control'/>
+                    </div>
+                    <div className='form-group'>
+                        <input type='text' placeholder='Book author' id='author' name='author'
+                               className='form-control'/>
+                    </div>
+                    <div className='form-group'>
+                        <textarea placeholder='Book description' id='description' name='description'
+                                  className='form-control'/>
+                    </div>
+                    <div className='form-group'>
+                        <input type='checkbox' id='availability' name='availability'
+                               className='form-check-input'/>
+                        <label for="availability" className='form-check-label'>Available</label>
+                        <div className='form-group'>
+                            <input type='text' placeholder='Book image' id='image' name='image'
+                                   className='form-control'/>
+                        </div>
+                        <button type='submit' className='btn btn-primary'>Add</button>
+                    </div>
+                </form>
             </div>
         )
     }

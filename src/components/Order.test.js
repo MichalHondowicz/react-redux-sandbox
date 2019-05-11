64f100643a872d57dd4b41ac7f5d1270ff8c961b
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-05-11 15:34:17 +0200

    Order component with test added

---
+++ src/components/Order.test.js
@@ -0,0 +1,20 @@
+import ReactDOM from "react-dom";
+import Order from "./Order";
+import React from 'react';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({adapter: new Adapter()});
+
+describe('Order test', () => {
+    it('Order renders with no problems ', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Order/>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('Inventory rendered', () => {
+        const wrapper = shallow(<Order/>);
+        expect(wrapper.find('div').text()).toBe('Order');
+    })
+});

8cabc1a1d89017e995236c60d2d711786a3a4d97
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-05-12 13:36:38 +0200

    Props passed to Inventory.js

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 class Inventory extends React.Component {
 
+
     render() {
+
+        const bookList = this.props.books.map(book => {
+           return <div>{book.name}</div>
+        });
         return (
             <div className='inventory col-md-4'>
-                Inventory
+                {bookList}
             </div>
         )
     }

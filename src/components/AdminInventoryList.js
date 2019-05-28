2242c849e77ae3a083ae3ae1c3d88271290c0302
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-05-28 19:33:04 +0200

    RemoveFromInventory button added to AdminPanel

--- src/components/AdminInventoryList.js
+++ src/components/AdminInventoryList.js
@@ -5,14 +5,11 @@ export default class AdminInventoryList extends React.Component {
 
 
     render() {
-        let inventoryList;
+        let inventoryList = <div>No books in inventory</div>;
         if (this.props.books) {
             inventoryList = this.props.books.map(book => {
                return <InventoryItem book={book} removeFromInventory={this.props.removeFromInventory}/>
-            })
-        } else {
-            inventoryList = <div>No books in inventory</div>
-        }
+            })}
         return (
             <div className='inventory col-md-6'>
                 <h2>Cart: </h2>

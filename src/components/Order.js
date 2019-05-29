80846bc9d6331b23b09beb8cace3d996185c924c
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-05-29 20:18:06 +0200

    Price summary added to Order

--- src/components/Order.js
+++ src/components/Order.js
@@ -4,13 +4,16 @@ import OrderItem from './OrderItem';
 class Order extends React.Component {
 
     render() {
-        const orderList = this.props.order.map(book => {
+        const orderList = this.props.order.map((book) => {
             return <OrderItem book={book} removeFromOrder={this.props.removeFromOrder}/>
         });
+        const orderSummary = this.props.order.map((book) => Number(book.price)).reduce((sum, next) => sum + next, 0);
+
         return (
             <div className='inventory col-md-6'>
                 <h2>Cart: </h2>
                 {orderList}
+                {orderSummary}
             </div>
         )
     }
--- src/components/OrderItem.js
+++ src/components/OrderItem.js
@@ -12,6 +12,7 @@ export default class OrderItem extends React.Component {
                 <div className='col-md-8'>
                     <b>{this.props.book.name}</b><br/>
                     <i>{this.props.book.author}</i><br/>
+                    <i>{this.props.book.price}</i><br/>
                 </div>
                 <div className='col-md-4'>
                     <button className='btn btn-danger' onClick={this.removeFromOrder}>Remove from Cart</button>
